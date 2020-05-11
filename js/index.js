7f76fa1e2d212e0f3b5e243149adca7baa8b0e97
mauro <mauricio.roldan@iatech.com.co>
2020-05-11 08:55:01 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,94 @@
+
+var burger = document.querySelector(".burger");
+var navLinks = document.querySelector(".nav-links");
+var navLinks_ = document.querySelectorAll(".nav-links li");
+burger.onclick = () => {
+    navLinks.classList.toggle("nav-clicked");
+
+    navLinks_.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = "";
+        } else {
+            link.style.animation = `navLinkAnimation 0.001s ease forwards ${index/5+0.5}s`;
+        }
+    });
+};
+
+
+//-------------- SLIDER 1 -------------------------
+
+const slider = document.querySelector(' .slider ');
+var sectionIndex = 0;
+
+document.querySelectorAll('.controls li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        document.querySelector('.controls .selected').classList.remove('selected');
+        indicator.classList.add ('selected');
+        slider.style.transform = 'translate(' + (ind) * -33.33 + '%)';    
+    });
+});
+
+
+//-------------- SLIDER 1_Menú -------------------------
+
+
+document.querySelectorAll('.controls_menu li').forEach(function(indicator, ind) {
+    indicator.addEventListener('click', function() {
+        sectionIndex = ind;
+        document.querySelector('.controls_menu .selected').classList.remove('selected');
+        indicator.classList.add ('selected');
+        slider.style.transform = 'translate(' + (ind) * -33.33 + '%)';    
+    });
+});
+
+//-------------- Show/hide class="container_two_one" -------------------------
+
+var btnshow = document.getElementById('btnShow');
+var cardshow = document.getElementById('cards');
+
+btnshow.addEventListener("click", show);
+
+function show(){
+    console.log('hola')
+    if (cardshow.style.display !== "flex"){
+        cardshow.style.display = "flex";
+    } else {
+        cardshow.style.display = "none";
+    }
+};
+
+//-------------- Increase/Decrease class="container_two_three" -------------------------
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
+
+
+  function increaseValueTwo() {
+    var value = parseInt(document.getElementById('numbertwo').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numbertwo').value = value;
+  }
+  
+  function decreaseValueTwo() {
+    var value = parseInt(document.getElementById('numbertwo').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('numbertwo').value = value;
+  }
+
+
+
